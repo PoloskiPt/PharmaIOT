be466799f668c41ae40dba4e70c5f0d8c2dadb50
Poloski <igorvsoarespt@gmail.com>
2022-06-09 15:44:41 +0100

    changes to notifications

--- screens/notificationsModal.js
+++ screens/notificationsModal.js
@@ -1,38 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView ,Modal, TouchableHighlight} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {Text, View, ScrollView ,Modal, TouchableHighlight} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { notificacoesStyles } from '../styles/global';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import LottieView from 'lottie-react-native';
 
 const NotificationModal = (props) => {
-    const notificationData = props.route.params.notificationsData;  
-   
+    const animation = useRef(null);
+    let notificationsExist = false;
     let notificationsArray = [];
-   
-    notificationData.map(element => {
-    
-    notificationsArray.push(
-        <View key={element['input']} style={notificacoesStyles.notificationCard}>
+    const notificationData = props.route.params.notificationsData;  
+        if(notificationData.message != "No data found"){
+            notificationsExist = true;
+            notificationData.map(element => {
             
-            <View style={notificacoesStyles.statusView}>
-                <Text style={notificacoesStyles.notificationSubtitleText}>Data: </Text> 
-                <Text>{element['dt']}</Text>
-            </View>
-
-            <View style={notificacoesStyles.statusView}>
-                <Text style={notificacoesStyles.notificationSubtitleText}>Mensagem: 
-                        {<Text style={{flexShrink: 1, fontSize:14}}> {element['message']}</Text>}
-                </Text>
-            </View>
-            
-            <View style={notificacoesStyles.statusView}>
-                <Text style={notificacoesStyles.notificationSubtitleText}>Status: </Text>
-                {element['solved'] === 1 ? <Text style={notificacoesStyles.statusResolved}>Resolvido</Text> : <Text style={notificacoesStyles.statusNotResolved}>Por resolver</Text>}
-            </View>
-            
-        </View>
-    )
-   }); 
+            notificationsArray.push(
+                <View key={element['input']} style={notificacoesStyles.notificationCard}>
+                    
+                    <View style={notificacoesStyles.statusView}>
+                        <Text style={notificacoesStyles.notificationSubtitleText}>Data: </Text> 
+                        <Text>{element['dt']}</Text>
+                    </View>
+        
+                    <View style={notificacoesStyles.statusView}>
+                        <Text style={notificacoesStyles.notificationSubtitleText}>Mensagem: 
+                                {<Text style={{flexShrink: 1, fontSize:14}}> {element['message']}</Text>}
+                        </Text>
+                    </View>
+                    
+                    <View style={notificacoesStyles.statusView}>
+                        <Text style={notificacoesStyles.notificationSubtitleText}>Status: </Text>
+                        {element['solved'] === 1 ? <Text style={notificacoesStyles.statusResolved}>Resolvido</Text> : <Text style={notificacoesStyles.statusNotResolved}>Por resolver</Text>}
+                    </View>
+                    
+                </View>
+            )
+           }); 
+           
+        }else{
+            console.log("data das notificacoes: " + JSON.stringify(notificationData));
+            notificationsExist = false;
+        }    
+        
+ 
+  
   
     return (  
 
@@ -47,14 +58,37 @@ const NotificationModal = (props) => {
                 </TouchableHighlight>
                 </View>
                 <ScrollView>
-                    {notificationsArray}
+                    
+                        
+            { !notificationsExist && <View style={{backgroundColor: '#fff',alignItems: 'center',justifyContent: 'center', flex: 1,}}>
+    
+                        <LottieView
+                                ref={animation}
+                                style={{
+                                height: 600,
+                                backgroundColor: 'transparent',
+                                }}
+                                loop={true}
+                                autoPlay={true}
+                                // Find more Lottie files at https://lottiefiles.com/featured
+                                source={require('../assets/no_notifications.json')}
+                        /> 
+            </View>   
+  } 
+           
+                       
+                    {notificationsExist && notificationsArray}
                 </ScrollView>
                 </View>   
                 </SafeAreaView>        
                 </Modal>    
             </View>
     );
+
+                         
+
 }
+
 
 export default NotificationModal;
 
