1496fb57384f5c8c39a5218d1860ffe4f8efafac
Poloski <igorvsoarespt@gmail.com>
2022-06-17 18:09:43 +0100

    notifications page added

--- routes/authUser/notificationsStack.js
+++ routes/authUser/notificationsStack.js
@@ -1,5 +1,5 @@
 import { createStackNavigator } from "react-navigation-stack";
-import Notifications from '../../screens/notificationsModal';
+import Notifications from '../../screens/notificationScreen';
 import Header from "../../shared/header";
 import React from 'react';
 
@@ -9,7 +9,7 @@ const screens = {
         screen: Notifications,
         navigationOptions: ({ navigation }) => {
             return {
-                header: () => <Header navigation ={navigation} title= 'Notificaçóes' tipo='titulo'/>,
+                header: () => <Header navigation ={navigation} title= 'Notificações' tipo='titulo'/>,
             }
         }
     },
