156584690bd9504566c6f3ce911b3ba184d92f63
Poloski <igorvsoarespt@gmail.com>
2022-06-17 16:18:31 +0100

    Changes to notifications

--- screens/notificationsModal.js
+++ screens/notificationsModal.js
@@ -1,10 +1,11 @@
-import React, {useRef, useState,useContext} from 'react';
-import {Text, View, ScrollView ,Modal, TouchableHighlight,TouchableOpacity,Image} from 'react-native';
+import React, {useRef, useState,useContext, useEffect} from 'react';
+import {Text, View, ScrollView ,Modal, TouchableHighlight,TouchableOpacity,Image,FlatList, Alert} from 'react-native';
 import {UserContext} from '../store/userContext';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { notificacoesStyles,loginStyles } from '../styles/global';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import LottieView from 'lottie-react-native';
+import {getNotifications} from '../functions/genericFunctions';
 
 const NotificationModal = (props) => {
     const animation = useRef(null);
@@ -12,10 +13,30 @@ const NotificationModal = (props) => {
     const {sessionDb} = useContext(UserContext);
     let notificationsExist = false;
     let notificationsArray = [];
-    const notificationData = props.route.params.notificationsData;  
+    const [notificationsData, setNotificationsData] = useState(props.route.params.notificationsData);
 
     const saveProfileDataUrl = "https://app.pharmaiot.pt/api/api/monitorizacao/update_alert_status.php";
    
+
+    const deleteSelectedElement = (id) => {
+ 
+        Alert.alert(
+          'Tem a certeza que pretende realizar esta operação? ',
+          'Selecione uma das opções',
+          [
+            { text: 'Cancelar', onPress: () => { } },
+            {
+              text: 'Sim', onPress: () => {
+                // Filter Data 
+                const filteredData = notificationsData.filter(item => item.input !== id);
+                //Updating List Data State with NEW Data.
+                setNotificationsData(filteredData);
+              }
+            },
+          ])
+      }
+
+
   async function updateInformation(id)  {
     response = await fetch(saveProfileDataUrl,{
         method: 'PUT',
@@ -25,57 +46,24 @@ const NotificationModal = (props) => {
         },
         body: JSON.stringify({
            
-          input:id,
+          input: id,
           solved: "1",
           db_name: sessionDb,
           username: sessionDb
         })
       
     });
-   
+    deleteSelectedElement(id);
     const data = await response.json()
+  
 }
 
-        if(notificationData.message != "No data found"){
-            notificationsExist = true;
-            notificationData.map(element => {
-            
-            notificationsArray.push(
-                
-                <View key={element['input']}   style={notificacoesStyles.notificationCard}>
-                    <TouchableOpacity onPress={() =>  updateInformation(element['input']) }>
-                   <View style={loginStyles.informacoesSection}>
-                   <Image style={loginStyles.loginIcons}source= {require('../assets/info.png')}/>
-                   <Text>{element['input']}</Text>
-                   </View> 
-                   </TouchableOpacity> 
-                    <View style={notificacoesStyles.statusView}>
-                        <Text style={notificacoesStyles.notificationSubtitleText}>Data: </Text> 
-                        <Text>{element['dt']}</Text>
-                    </View>
-        
-                    <View style={notificacoesStyles.statusView}>
-                        <Text style={notificacoesStyles.notificationSubtitleText}>Mensagem: 
-                                {<Text style={{flexShrink: 1, fontSize:14}}> {element['message']}</Text>}
-                        </Text>
-                    </View>
-                    
-                    <View style={notificacoesStyles.statusView}>
-                        <Text style={notificacoesStyles.notificationSubtitleText}>Status: </Text>
-                        {element['solved'] === 1 ? <Text style={notificacoesStyles.statusResolved}>Resolvido</Text> : <Text style={notificacoesStyles.statusNotResolved}>Por resolver</Text>}
-                    </View>
-                    
-                </View>
-            )
-           }); 
-           
-        }else{
-            console.log("data das notificacoes: " + JSON.stringify(notificationData));
-          
+        if(notificationsData.message != "No data found"){
+            notificationsExist = true;           
+            console.log(notificationsData);
+        }else{       
             notificationsExist = false;
         }    
-        
-        console.log(input)
   
   
     return (  
@@ -83,6 +71,7 @@ const NotificationModal = (props) => {
             <View >
                 <Modal animationType="slide" 
                 Visible={props.modalState} >      
+                
                 <SafeAreaView>
                 <View style={{display:'flex', height:'100%'}} >
                 <View style={notificacoesStyles.closeIcon}>
@@ -90,9 +79,8 @@ const NotificationModal = (props) => {
                 <Icon name='close-outline' style={{color:'black'}} size={40}  type="Ionicons" onPress={() => props.navigation.pop()}/>
                 </TouchableHighlight>
                 </View>
-                <ScrollView>
-                    
-                        
+                
+
             { !notificationsExist && <View style={{backgroundColor: '#fff',alignItems: 'center',justifyContent: 'center', flex: 1,}}>
     
                         <LottieView
@@ -109,11 +97,49 @@ const NotificationModal = (props) => {
             </View>   
   } 
            
-                       
-                    {notificationsExist && notificationsArray}
-                </ScrollView>
+                {notificationsExist && 
+                
+                <SafeAreaView>
+                
+             <FlatList
+                 keyExtractor={(item) => item.input}
+                 data={notificationsData}
+                 renderItem={({item}) =>               
+                 <View key={item['input']}   style={notificacoesStyles.notificationCard}>
+                 <TouchableOpacity onPress={() =>  updateInformation(item['input']) && deleteSelectedElement(item['input'])}>
+                <View style={loginStyles.informacoesSection}>
+                <Image style={loginStyles.loginIcons}source= {require('../assets/info.png')}/>
+                <Text>{item['input']}</Text>
+                </View> 
+                </TouchableOpacity> 
+                 <View style={notificacoesStyles.statusView}>
+                     <Text style={notificacoesStyles.notificationSubtitleText}>Data: </Text> 
+                     <Text>{item['dt']}</Text>
+                 </View>
+     
+                 <View style={notificacoesStyles.statusView}>
+                     <Text style={notificacoesStyles.notificationSubtitleText}>Mensagem: 
+                             {<Text style={{flexShrink: 1, fontSize:14}}> {item['message']}</Text>}
+                     </Text>
+                 </View>
+                 
+                 <View style={notificacoesStyles.statusView}>
+                     <Text style={notificacoesStyles.notificationSubtitleText}>Status: </Text>
+                     {item['solved'] === 1 ? <Text style={notificacoesStyles.statusResolved}>Resolvido</Text> : <Text style={notificacoesStyles.statusNotResolved}>Por resolver</Text>}
+                 </View>
+                 
+             </View>
+             }
+             />
+     
+     </SafeAreaView>         
+
+                }       
+                
+            
                 </View>   
-                </SafeAreaView>        
+                </SafeAreaView>   
+                    
                 </Modal>    
             </View>
     );
